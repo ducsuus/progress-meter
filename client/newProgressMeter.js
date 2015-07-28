91c03c8480f82c5787bd84b9480183d147db874a
JoshuaMankelow <Josh@Joshuas-MacBook.local>
2015-07-28 12:29:31 +0100

    Added functionality for adding new stages

--- client/newProgressMeter.js
+++ client/newProgressMeter.js
@@ -3,7 +3,9 @@ var stages = [];
 //[[name, description]]
 
 function addNewStage(){
-	stages.push(['dsadsadassadasd', 'dsaasddasdas']);
+	stageID = stages.length;
+
+	stages.push(['Name', 'Description']);
 	updateStages();
 }
 
@@ -18,10 +20,23 @@ function updateStages() {
 
 	var stageHTML = ''
 
+	console.log(stages);
+
 	for (var i = 0; i < stages.length; i++){
-		stageHTML += '<div class="edit-stage"><div class="form-group"><label class="form-label" for="stageName">Stage ' + (i + 1) + '</label>		<button id="deleteButton' + i + '" class="btn btn-default button-style" type="button" onclick="deleteStage(' + i + ')">Delete Stage</button>		<input type="text" class="form-control stage-form" placeholder="Stage ' + (i + 1) + ' Name">	</div>	<div class="form-group">		<input type="text" class="form-control stage-form" stageDesc="stageDesc" placeholder="Description">	</div></div>';
+		console.log(stages[i][1]);
+		stageHTML += '<div class="edit-stage"><div class="form-group"><label class="form-label" for="stageName">Stage ' + (i + 1) + '</label><button id="deleteButton' + i + '" class="btn btn-default button-style" type="button" onclick="deleteStage(' + i + ')">Delete Stage</button><input onchange="updateStageData(\'name\', ' + stageID + ')" id="name-' + stageID + '" type="text" class="form-control stage-form" value="' + stages[i][0] + '" placeholder="Stage ' + (i + 1) + ' Name"></div><div class="form-group"><input id="description-' + stageID + '" type="text" value="' + stages[i][1] + '" class="form-control stage-form" stageDesc="stageDesc" onchange="updateStageData(\'description\', ' + stageID + ')" value="descData' + stageID + '" placeholder="Description">	</div></div>';
 	}
 
 	stageContainer.innerHTML = stageHTML;
 	
+}
+
+function updateStageData(name, id) {
+	
+	var properties = ['name', 'description'];
+
+	var inputProperty = document.getElementById(name + '-' + id);
+
+	stages[id][properties.indexOf(name)] = inputProperty.value;
+
 }
