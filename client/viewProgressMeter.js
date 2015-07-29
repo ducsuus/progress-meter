c7091d85b38ce0f4297568486aaff5d106c10441
JoshuaMankelow <Josh@Joshuas-MacBook.local>
2015-07-29 11:45:52 +0100

    Added a progress bar that only displays 4 stages to one page width

--- client/viewProgressMeter.js
+++ client/viewProgressMeter.js
@@ -7,9 +7,9 @@ function getStages(){
     var xhr = new XMLHttpRequest();
 
     // Define a callback function to be called when we have our JSON from the server
-    xhr.onreadystatechange=function(){
+    xhr.onreadystatechange=function() {
         // Received a response from the server
-        if (xhr.readyState==4 && xhr.status==200){
+        if (xhr.readyState==4 && xhr.status==200) {
             console.log('Received a response: ' + xhr.responseText);
 
             // Decode the JSON response
@@ -18,18 +18,35 @@ function getStages(){
             console.log(response);
 
             var stageContainer = document.getElementById('stage-list');
+            var stageContainerWidth = document.getElementById('stage-list').offsetWidth;
+            var minWidth = stageContainerWidth/4;
+            var possWidth = stageContainerWidth/response.length;
 
-            for (var x = 0; x < response.length; x++){
+            if (stageContainerWidth/response.length >= minWidth) {
+                for (var x = 0; x < response.length; x++){
                 
-                stageTitle = response[x]['title'];
-                stageInformation = response[x]['information'];
-                stageCompletion = response[x]['complete'];
-                if (stageCompletion == 0) {
-                    stageCompletion = 'No'
-                } else {
-                    stageCompletion = 'Yes'
+                    stageTitle = response[x]['title'];
+                    stageInformation = response[x]['information'];
+                    stageCompletion = response[x]['complete'];
+                    if (stageCompletion == 0) {
+                        stageCompletion = 'No'
+                    } else {
+                        stageCompletion = 'Yes'
+                    }
+                    stageContainer.innerHTML += '<div class="Progress" style="width: ' + possWidth + '"><div class="small-form-label-black">Stage ' + (x + 1) + ' ' + stageTitle + '<br>' + stageInformation + '<br>Stage complete : ' + stageCompletion + '</div>';
                 }
-                stageContainer.innerHTML += '<div class="subtitle">Stage ' + (x + 1) + ' ' + stageTitle + '</div><div class="small-form-label">' + stageInformation + '</div><div class="small-form-label">Stage complete : ' + stageCompletion + '</div>';
+            } else {
+                for (var x = 0; x < response.length; x++) {
+                    stageTitle = response[x]['title'];
+                    stageInformation = response[x]['information'];
+                    stageCompletion = response[x]['complete'];
+                    if (stageCompletion == 0) {
+                        stageCompletion = 'No';
+                    } else {
+                        stagecompletion = 'Yes';
+                    }
+                    stageContainer.innerHTML += '<div class="Progress" style="width: ' + minWidth + '"><div class="small-form-label-black">Stage ' + (x + 1) + ' ' + stageTitle + '<br>' + stageInformation + '<br>Stage complete : ' + stageCompletion + '</div>';
+                }
             }
         }
     }
