c081fbc7172ff77af5335eaa2e151a6811194b8a
JoshuaMankelow <Josh@Joshuas-MacBook.local>
2015-07-30 10:30:42 +0100

    Made the input fields display inline

--- client/editProgressMeterMK2.js
+++ client/editProgressMeterMK2.js
@@ -1,13 +1,13 @@
 // View Progress Meter JavaScript
 
-function drawStages(stages){
+function drawStages(stages) {
 
     var stagesContainer = document.getElementById('stage-container');
 
     var width = stagesContainer.offsetWidth;
 
     // Max 4 stages per row
-    if (stages.length > 4){
+    if (stages.length > 4) {
         // The width of each stage, account margin around it
         var stageWidth = (width - (5 * 2 * 4)) / 4;
     } else{
@@ -19,11 +19,10 @@ function drawStages(stages){
     for(var i = 0; i < stages.length; i++){
         if(stages[i]['complete'] == '1'){
             // Stage complete, display it so
-            htmlString += '<div><input style=\'width: ' + stageWidth + ';\' class=\'progress stage-complete form-control\' value="' + stages[i]['title'] + '"><br><input style=\'width: ' + stageWidth + ';\' class=\'progress stage-uncomplete form-control\' value="' + stages[i]['information'] + '"></div>';
-        }
-        else{
+            htmlString += '<div style=\'display: inline-block;\' class="custom-input-container"><input style=\'width: ' + stageWidth + 'px;\' class=\'progress stage-complete form-control\' value="' + stages[i]['title'] + '"><br><input style=\'width: ' + stageWidth + ';\' class=\'progress stage-complete form-control\' value="' + stages[i]['information'] + '"></div>';
+        } else {
             // Stage not complete, display it so
-            htmlString += '<div><input style=\'width: ' + stageWidth + ';\' class=\'progress stage-uncomplete form-control\' value="' + stages[i]['title'] + '"><br><input style=\'width: ' + stageWidth + ';\' class=\'progress stage-uncomplete form-control\' value="' + stages[i]['information'] + '""></div>';            
+            htmlString += '<div style=\'display: inline-block;\' class="custom-input-container"><input style=\'width: ' + stageWidth + 'px;\' class=\'progress stage-uncomplete form-control\' value="' + stages[i]['title'] + '"><br><input style=\'width: ' + stageWidth + ';\' class=\'progress stage-complete form-control\' value="' + stages[i]['information'] + '"></div>';         
         }
 
         //htmlString += '<div style=\'width: ' + stageWidth + ';\' class=\'progress\'>' + stages[i]['title'] + '<br>' + stages[i]['information'] + '</div>';            
@@ -46,7 +45,6 @@ function getStages(){
     xhr.onreadystatechange=function() {
         // Received a response from the server
         if (xhr.readyState==4 && xhr.status==200) {
-            console.log('Received a response: ' + xhr.responseText);
 
             // Decode the JSON response
             var response = JSON.parse(xhr.responseText);
